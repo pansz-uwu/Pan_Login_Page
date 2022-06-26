ffda0e673516005a3a37d3a9b8053fbd99529d9f
pan <cesarsoarespr@gmail.com>
2022-06-26 16:49:11 -0300

    main

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const inputs = document.querySelectorAll('.input');
-const button = document.querySelector('.button_login');
+// const inputs = document.querySelectorAll('.input');
+// const button = document.querySelector('.button_login');
 
-const handleChange = () => {
-    const [username, password] = inputs;
+// const handleChange = () => {
+//     const [username, password] = inputs;
 
-    if (username.value && password.value >= 8){
-        button.removeAttribute('disabled');
-    } else {
-        button.setAttribute('disabled', '');
-    }
-}
-inputs.forEach(input) => input.addEventListener('input', handleChange);
+//     if (username.value && password.value >= 8){
+//         button.removeAttribute('disabled');
+//     } else {
+//         button.setAttribute('disabled', '');
+//     }
+// }
+// inputs.forEach(input) => input.addEventListener('input', handleChange);
